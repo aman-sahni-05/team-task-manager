bee38fc7b3774698a722824695aca9e0e39f2a15
Aman <aman.sahni052001@gmail.com>
2026-06-24 08:05:42 +0530

    feat: error handler middleware added for unexpected errors

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const app = express()
 const authRoutes = require('./routes/authRoute.js')
 const taskRoutes = require('./routes/taskRoutes.js')
 const projectRoutes = require('./routes/projectsRoute.js')
+const errorHandler = require('./middleware/errorMiddleware.js')
 app.use(express.json())
 
 app.use('/auth', authRoutes) //Login and Register
@@ -12,6 +13,8 @@ app.use('/tasks', taskRoutes) // Tasks CRUD
 
 app.use('/projects', projectRoutes) // Project CRUD
 
+//error handler
+app.use(errorHandler)
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on port ${process.env.PORT}`)
