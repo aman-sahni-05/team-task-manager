f10edad83f32ae044ed91b0d837f6774f71a8f2b
Aman <aman.sahni052001@gmail.com>
2026-06-25 08:13:07 +0530

    feat: Project member controller and routes created

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,6 +1,7 @@
 const e = require('express')
 const pool = require('../config/db.js')
 
+// PROJECT CREATION, DELETION, UPDATION AND INSERTION
 async function createProject(req,res,next) {
     try{
         const userId = req.user.id
@@ -48,4 +49,58 @@ async function deleteProject(req,res,next) {
 }
 
 
-module.exports = {createProject,allProjects,updateProject,deleteProject}
+
+
+// PROJECT MEMBERS INSERTION AND DELETION
+
+async function addMembers(req,res,next) {
+    try{
+        const loggedInUser = req.user.id
+        const projectId = req.params.id
+        const userId = req.body.user_id
+
+        if(!userId){
+            return res.status(400).json({message: "User id is required"})
+        }
+
+        const projectResult = await pool.query(`SELECT created_by FROM projects WHERE id=$1`,[projectId])
+        if(projectResult.rows.length === 0){
+            return res.status(404).json({message: "Project not found"})
+        }
+        if(String(projectResult.rows[0].created_by) !== String(loggedInUser)){
+            return res.status(403).json({message: "Only Project creator can add members"})
+        }
+        const insertedMember = await pool.query(`INSERT INTO project_members (project_id,user_id) VALUES ($1,$2) RETURNING *`, [projectId,userId])
+        res.status(201).json(insertedMember.rows[0])
+
+    }catch(err){
+        next(err)
+    }
+}
+
+async function removeMembers(req,res,next) {
+    try{
+        const loggedInUser = req.user.id
+        const userId = req.body.user_id
+        const projectId = req.params.id
+
+        const projectResult = await pool.query(`SELECT created_by FROM projects WHERE id=$1`,[projectId])
+        if(projectResult.rows.length === 0){
+            return res.status(404).json({message: "Project not found"})
+        }
+        if(String(projectResult.rows[0].created_by) !== String(loggedInUser)){
+            return res.status(403).json({message: "Only project creator can add members"})
+        }
+        const removedMembers = await pool.query(`DELETE FROM project_members WHERE user_id=$1 AND project_id=$2 RETURNING *`,[userId,projectId])
+        if(removedMembers.rowCount === 0){
+            return res.status(404).json({message: "Member not found in this project"})
+        }
+        res.json({message: `Project member ${removedMembers.rows[0].user_id} is removed`})
+    }catch(err){
+        next(err)
+    }
+}
+
+
+
+module.exports = {createProject,allProjects,updateProject,deleteProject,addMembers,removeMembers}
--- backend/routes/projectsRoute.js
+++ backend/routes/projectsRoute.js
@@ -1,11 +1,17 @@
 const { Router } = require("express")
 const router = Router()
 const verifyToken = require('../middleware/authMiddleware.js')
-const {createProject,allProjects,updateProject,deleteProject} = require('../controllers/projectController.js')
+const {createProject,allProjects,updateProject,deleteProject,addMembers,removeMembers} = require('../controllers/projectController.js')
 
+
+// PROJECT ROUTES
 router.post('/', verifyToken,createProject)
 router.get('/', verifyToken, allProjects)
 router.put('/:id', verifyToken,updateProject)
 router.delete('/:id', verifyToken,deleteProject)
+
+// PROJECT MEMBERS ROUTES
+router.post('/:id/members', verifyToken, addMembers)
+router.delete('/:id/members', verifyToken, removeMembers)
 
 module.exports = router
