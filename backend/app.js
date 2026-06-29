fde186be2b9b43eb89f5e718c193df74729ad8e0
Aman <aman.sahni052001@gmail.com>
2026-06-30 05:28:14 +0530

    fix: backend cors fixed by downloading and setup cors

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,12 @@ const authRoutes = require('./routes/authRoute.js')
 const taskRoutes = require('./routes/taskRoutes.js')
 const projectRoutes = require('./routes/projectsRoute.js')
 const errorHandler = require('./middleware/errorMiddleware.js')
+const cors = require('cors')
+
+app.use(cors({
+    origin: "http://localhost:5173"   //cross-origin resource sharing
+}))
+
 app.use(express.json())
 
 app.use('/auth', authRoutes) //Login and Register
