f00e62a5d2a0debdba84891b33ac3ee365f12069
Aman <aman.sahni052001@gmail.com>
2026-07-10 06:24:32 +0530

    fix: jwt error return 401 instead of 500

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -2,6 +2,9 @@
 
 function errorHandler(err,req,res,next){
     console.log(err)
+    if(err.name === 'TokenExpiredError' || err.name === 'JsonWebTokenError') {
+        return res.status(401).json({ message: err.message })
+    }
     res.status(err.status || 500).json({message: err.message})
 }
 
